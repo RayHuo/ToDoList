7fbb309f899985b77a84b2bb30d3e597de1490b4
RayHuo <rayhuo_81@hotmail.com>
2015-02-17 22:35:18 +0800

    完整SQLite

--- gen/com/rayhuo/todolist/R.java
+++ gen/com/rayhuo/todolist/R.java
@@ -24,14 +24,14 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int Spinner01=0x7f080005;
+        public static final int TextView01=0x7f080000;
+        public static final int TextView02=0x7f080006;
         public static final int action_settings=0x7f080007;
-        public static final int add_button=0x7f080000;
-        public static final int button1=0x7f080002;
-        public static final int button2=0x7f080004;
-        public static final int button3=0x7f080006;
-        public static final int delete_button=0x7f080001;
+        public static final int add_button=0x7f080001;
+        public static final int delete_button=0x7f080002;
         public static final int modify_button=0x7f080003;
-        public static final int query_button=0x7f080005;
+        public static final int query_button=0x7f080004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- src/com/rayhuo/todolist/MainActivity.java
+++ src/com/rayhuo/todolist/MainActivity.java
@@ -2,9 +2,19 @@ package com.rayhuo.todolist;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.Button;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	
@@ -32,16 +42,135 @@ public class MainActivity extends Activity {
 		
 		// 更新下拉列表中的数据
 		updateSpinner();
+		
+		// 定义按钮点击监听器
+		OnClickListener ocl = new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+				// ContentValues对象
+				ContentValues cv = new ContentValues();
+				
+				switch (v.getId()) {
+					case R.id.add_button:
+						cv.put("filename", "pic5.jpg");
+						cv.put("description", "picture 5");
+						
+						long long1 = db.insert("pic", "", cv);
+						// 添加成功后返回行号，失败后返回-1
+						if(long1 == -1) {
+							Toast.makeText(MainActivity.this, "ID " + long1 + ", Add failed", Toast.LENGTH_SHORT).show();
+						}
+						else {
+							Toast.makeText(MainActivity.this, "ID " + long1 + ", Add Successfully", Toast.LENGTH_SHORT).show();
+						}
+						
+						// 更新一下下拉列表
+						updateSpinner();
+						break;
+						
+					case R.id.delete_button:
+						long long2 = db.delete("pic", "description='picture 5'", null);
+						// 删除失败后返回0，成功则返回删除的条数
+						Toast.makeText(MainActivity.this, "Delete " + long2 + " items", Toast.LENGTH_SHORT).show();
+						updateSpinner();
+						break;
+						
+					case R.id.modify_button:
+						cv.put("filename", "pic0.jpg");
+						cv.put("description", "picture 0");
+						int long3 = db.update("pic", cv, "filename='pic.jpg'", null);
+						Toast.makeText(MainActivity.this, "Update " + long3 + " items", Toast.LENGTH_SHORT).show();
+						updateSpinner();
+						break;
+						
+					case R.id.query_button:
+						// 这个Cursor（游标）是指向返回的记录数组，查询可能返回一系列的结果，游标的功能类似迭代器
+						Cursor c = db.query("pic", null, null, null, null, null, null);
+						Toast.makeText(MainActivity.this, "Get total " + c.getCount() + " items. As following: ", Toast.LENGTH_SHORT).show();
+						
+						for(c.moveToFirst(); !c.isAfterLast(); c.moveToNext()) {
+							Toast.makeText(MainActivity.this, "The " + c.getInt(0) + " item. filename is " + c.getString(1) + ", description is " + c.getString(2), Toast.LENGTH_SHORT).show();
+						}
+						
+						updateSpinner();
+						break;
+						
+					default:
+						break;
+				}
+			}
+		};
+		
+		addButton.setOnClickListener(ocl);
+		deleteButton.setOnClickListener(ocl);
+		modifyButton.setOnClickListener(ocl);
+		queryButton.setOnClickListener(ocl);
+		
 	}
 
+	@SuppressWarnings("deprecation")
 	private void updateSpinner() {
 		// TODO Auto-generated method stub
 		
+		// 定义UI组件
+		final TextView tv = (TextView)findViewById(R.id.TextView02);
+		Spinner s = (Spinner)findViewById(R.id.Spinner01);
+		
+		//从数据库中获取数据放入游标Cursor对象
+		final Cursor cursor = db.query("pic", null, null, null, null, null, null);
+		
+		//创建简单游标匹配器
+		SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
+				                android.R.layout.simple_spinner_item, cursor, new String[] {
+				                        "fileName", "description" }, new int[] {
+				                        android.R.id.text1, android.R.id.text2 });
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		
+		//给下拉列表设置匹配器
+		s.setAdapter(adapter);
+		
+		//定义子元素选择监听器
+		OnItemSelectedListener oisl = new OnItemSelectedListener() {
+
+			@Override
+			public void onItemSelected(AdapterView<?> arg0, View view,
+					int position, long id) {
+				// TODO Auto-generated method stub
+				cursor.moveToPosition(position);
+				tv.setText("当前pic的描述为：" + cursor.getString(2));
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		};
 	}
 
-	private void initDatabase(SQLiteDatabase db2) {
+	private void initDatabase(SQLiteDatabase db) {
 		// TODO Auto-generated method stub
+		ContentValues cv = new ContentValues();
 		
+		cv.put("filename", "pic1.jpg");
+		cv.put("description", "picture 1");
+		db.insert(table_name, "", cv);
+		
+		cv.put("filename", "pic2.jpg");
+		cv.put("description", "picture 2");
+		db.insert(table_name, "", cv);
+		
+		cv.put("filename", "pic3.jpg");
+		cv.put("description", "picture 3");
+		db.insert(table_name, "", cv);
+		
+		cv.put("filename", "pic4.jpg");
+		cv.put("description", "picture 4");
+		db.insert(table_name, "", cv);
 	}
 
 	@Override
