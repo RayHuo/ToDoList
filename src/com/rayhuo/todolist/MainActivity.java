b4e98e446de498f7710f4c92feb2866d2ea4921f
RayHuo <rayhuo_81@hotmail.com>
2015-02-18 16:25:30 +0800

    使用ListView显示联系人信息

--- src/com/rayhuo/todolist/MainActivity.java
+++ src/com/rayhuo/todolist/MainActivity.java
@@ -1,6 +1,10 @@
 package com.rayhuo.todolist;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.os.Bundle;
+import android.provider.Contacts.People;
 import android.provider.ContactsContract;
 import android.provider.ContactsContract.PhoneLookup;
 import android.app.Activity;
@@ -13,6 +17,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListAdapter;
@@ -31,61 +36,31 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		m_LinearLayout = new LinearLayout(this);
-		m_LinearLayout.setOrientation(LinearLayout.VERTICAL);
-		m_LinearLayout.setBackgroundColor(Color.BLACK);
+//		setContentView(R.layout.activity_main);  
 		
 		m_ListView = new ListView(this);
-		LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-		m_ListView.setBackgroundColor(Color.BLACK);
+//		m_ListView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1,getData()));
+        
+		Cursor cursor = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+		startManagingCursor(cursor);
 		
-		m_LinearLayout.addView(m_ListView, param);
+		ListAdapter listAdapter = new SimpleCursorAdapter(this, 
+				android.R.layout.simple_list_item_2, 
+                cursor, 
+                new String[] { ContactsContract.Contacts.DISPLAY_NAME },
+                new int[] {android.R.id.text1});
+		m_ListView.setAdapter(listAdapter);
 		
-		setContentView(m_LinearLayout);
-		
-		Cursor cur = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
-		Toast.makeText(MainActivity.this, "Get " + cur.getCount() + " items.", Toast.LENGTH_SHORT).show();
-		//		startManagingCursor(cur);
-//		ListAdapter adapter = new SimpleCursorAdapter(this, 
-//				android.R.layout.simple_list_item_2,
-//				cur,
-//				new String[] { PhoneLookup.DISPLAY_NAME, PhoneLookup.NUMBER},
-//				new int[] {android.R.id.text1, android.R.id.text2});
-//		
-//		m_ListView.setAdapter(adapter);
-//		m_ListView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-//			@Override
-//			public void onItemSelected(AdapterView<?> arg0, View arg1,
-//					int arg2, long arg3) {
-//				// TODO Auto-generated method stub
-//				Toast.makeText(MainActivity.this, "Roll to " + Long.toString(arg0.getSelectedItemId()) + " item.", Toast.LENGTH_SHORT).show();
-//			}
-//
-//			@Override
-//			public void onNothingSelected(AdapterView<?> arg0) {
-//				// TODO Auto-generated method stub
-//				
-//			}
-//		});
-//		
-//		m_ListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//
-//			@Override
-//			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
-//					long arg3) {
-//				// TODO Auto-generated method stub
-//				Toast.makeText(MainActivity.this, "Click to " + Integer.toString(arg2+1) + " item.", Toast.LENGTH_SHORT).show();
-//			}
-//			
-//		});
-	}
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		setContentView(m_ListView);
 	}
 
+	private List<String> getData(){       
+        List<String> data = new ArrayList<String>();
+        data.add("测试数据1");
+        data.add("测试数据2");
+        data.add("测试数据3");
+        data.add("测试数据4");
+         
+        return data;
+    }
 }
